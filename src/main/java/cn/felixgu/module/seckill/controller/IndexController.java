1cfbc8571b67f1fc0b2dd1101a0adbb20873bd1f
felix <felix_gu@outlook.com>
2018-01-05 18:12:58 +0800

    init web application

--- src/main/java/cn/felixgu/module/seckill/controller/IndexController.java
+++ src/main/java/cn/felixgu/module/seckill/controller/IndexController.java
@@ -1,4 +1,25 @@
 package cn.felixgu.module.seckill.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@RestController
 public class IndexController {
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public Object index() {
+        HashMap<String, String> hash= new HashMap<String, String>(1);
+        hash.put("test","test");
+        return hash;
+    }
+
+    @RequestMapping(value = "/")
+    public Object test() {
+        return new ArrayList<String>().add("asd");
+    }
 }
